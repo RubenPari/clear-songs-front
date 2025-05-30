dd7d11d6484d8a234362cd6808383dd57b088482
Ruben Pari <rubenpari.developer@gmail.com>
2025-05-30 12:59:21 +0200

    modified app.component for testing artist-summary component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {ArtistSummaryComponent} from '../artist-summary/artist-summary.component';
+import ArtistSummary from '../artist-summary/artist-summary.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArtistSummaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'clear-songs-front';
+  artistSummary: ArtistSummary = {
+    name: 'Eminem',
+    imageUrl: 'https://yve.ro/wp-content/uploads/2017/10/18-lucruri-interesante-despre-Eminem.jpg',
+    tracksCount: 4
+  }
 }
