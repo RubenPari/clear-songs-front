05b4a44514fefc826db74c7eeeb6a18ad3ca3cf0
Ruben Pari <ruben.pari@lasersoft.it>
2025-08-13 18:04:12 +0200

    aggiunti file di environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  spotifyClientId: 'YOUR_SPOTIFY_CLIENT_ID',
+  spotifyRedirectUri: 'http://localhost:4200/callback',
+};
