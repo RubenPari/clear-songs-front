d8f524fee0c82b4d030f7bad6845f453ab6770b3
RubenPari <rubenpari.developer@gmail.com>
2025-08-21 10:23:50 +0200

    chore(env): add Angular environments (dev, prod)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-production-api.com',
+  spotifyClientId: 'YOUR_SPOTIFY_CLIENT_ID',
+  spotifyRedirectUri: 'https://your-production-url.com/callback'
+};
