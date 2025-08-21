40399b0f6d898b5daef2c15a0710634350659a37
RubenPari <rubenpari.developer@gmail.com>
2025-08-21 10:23:50 +0200

    feat(app): add root app shell (component, routes, template, styles)

---
+++ src/app/app.routes.ts
@@ -0,0 +1,47 @@
+import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { CallbackComponent } from './features/auth/callback/callback.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { PlaylistsComponent } from './features/playlists/playlists.component';
+import { TrackManagementComponent } from './features/tracks/track-management/track-managment.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'callback',
+    component: CallbackComponent,
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'tracks',
+        component: TrackManagementComponent,
+      },
+      {
+        path: 'playlists',
+        component: PlaylistsComponent,
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard',
+  },
+];
